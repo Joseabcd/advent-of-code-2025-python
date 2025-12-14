c11f10f38c9b31a0dbb105e065e178299dce540a
Jose Antonio Fernandez Vicente <jose.a.f.alm@gmail.com>
2025-12-14 05:48:38 +0100

    Add solution for day 3 part 1.

---
+++ day_3/day_3.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+input_path = os.path.join(script_dir, "input.txt")
+
+with open(input_path, "r") as f:
+    data = f.read()
+
+bank_list = data.split()
+
+
+# ------------------------------------------------------------
+# Part 1
+# ------------------------------------------------------------
+
+total_jolts = 0
+for bank in bank_list:
+  p0, p1 = 0, 1
+  for p in range(1, len(bank)):
+    if bank[p] > bank[p0] and p < (len(bank) - 1):
+      p0 = p
+      p1 = p0 + 1
+    elif bank[p] > bank[p1]:
+      p1 = p
+      if bank[p1] == 9:
+        break
+  cur_jolts = int(str(bank[p0]) + str(bank[p1]))
+  total_jolts += cur_jolts
+
+print(total_jolts)
+
+
+# ------------------------------------------------------------
+# Part 2
+# ------------------------------------------------------------
+
