c322ad06fe48db723ac6cc97d40475271c9ef4ce
Jose Antonio Fernandez Vicente <jose.a.f.alm@gmail.com>
2025-12-25 01:35:39 +0100

    Encapsulate the solution for day 4 part 1 in a function, and use it to solve part 2.

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -9,40 +9,49 @@ with open(input_path, "r") as f:
 
 mask = [[int(c.replace('@', '1').replace('.', '0')) for c in line] for line in data.split()]
 
+def accessible_rolls(mask, remove_in_place=False):
+  accessible_count = 0
+  m, n = len(mask), len(mask[0])
+  for i in range(m):
+    for j in range(n):
+      if mask[i][j]:
+        adjacent_count = 0
+        if i > 0:
+          adjacent_count += mask[i - 1][j]
+          if j > 0:
+            adjacent_count += mask[i - 1][j - 1]
+          if j < n - 1:
+            adjacent_count += mask[i - 1][j + 1]
+        if i < m - 1:
+          adjacent_count += mask[i + 1][j]
+          if j > 0:
+            adjacent_count += mask[i + 1][j - 1]
+          if j < n - 1:
+            adjacent_count += mask[i + 1][j + 1]
+        if j > 0:
+          adjacent_count += mask[i][j - 1]
+        if j < n - 1:
+          adjacent_count += mask[i][j + 1]
+        if adjacent_count < 4:
+          accessible_count += 1
+          if remove_in_place:
+            mask[i][j] = 0
+  return accessible_count
+
 
 # ------------------------------------------------------------
 # Part 1
 # ------------------------------------------------------------
 
-accessible_rolls = 0
-m, n = len(mask), len(mask[0])
-for i in range(m):
-  for j in range(n):
-    if mask[i][j]:
-      adjacent_rolls = 0
-      if i > 0:
-        adjacent_rolls += mask[i - 1][j]
-        if j > 0:
-          adjacent_rolls += mask[i - 1][j - 1]
-        if j < n - 1:
-          adjacent_rolls += mask[i - 1][j + 1]
-      if i < m - 1:
-        adjacent_rolls += mask[i + 1][j]
-        if j > 0:
-          adjacent_rolls += mask[i + 1][j - 1]
-        if j < n - 1:
-          adjacent_rolls += mask[i + 1][j + 1]
-      if j > 0:
-        adjacent_rolls += mask[i][j - 1]
-      if j < n - 1:
-        adjacent_rolls += mask[i][j + 1]
-      
-      if adjacent_rolls < 4:
-        accessible_rolls += 1
-      
-print(accessible_rolls)
+print(accessible_rolls(mask))
 
 
 # ------------------------------------------------------------
 # Part 2
 # ------------------------------------------------------------
+
+total_accessible_rolls = 0
+while (current_accessible_rolls := accessible_rolls(mask, remove_in_place=True)):
+  total_accessible_rolls += current_accessible_rolls
+print(total_accessible_rolls)
+
